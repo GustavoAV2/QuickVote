69c92e0db74818b03c8723d5bd87c9b45566b8f8
gustavo.voltolini <1Cachorro.grande>
2021-09-10 15:28:42 -0300

    Correcting imports

--- chat/routing.py
+++ chat/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from .consumers import ChatConsumer
+from chat.consumers import ChatConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/room/(?P<number>\w+)/$', ChatConsumer)
