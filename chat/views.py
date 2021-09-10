3ce2dbd46896f5f9148712d87e5b2dafced24888
gustavo.voltolini <1Cachorro.grande>
2021-09-10 10:16:21 -0300

    Correcting RoomView context

--- chat/views.py
+++ chat/views.py
@@ -8,13 +8,13 @@ class IndexView(TemplateView):
 
 
 class RoomView(TemplateView):
-    template_name = 'room.html'
+    template_name = 'chat.html'
 
     def get_context_data(self, **kwargs):
         context = super(RoomView, self).get_context_data(**kwargs)
 
         # Removendo inseguranças do JSON
-        context['room_json'] = mark_safe(
+        context['room'] = mark_safe(
             json.dumps(self.kwargs)
         )
         return context
