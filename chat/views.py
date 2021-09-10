e7c40f17cac524c1aafad231762c1d46209b0f87
gustavo.voltolini <1Cachorro.grande>
2021-09-10 09:33:15 -0300

    Creating Views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.utils.safestring import mark_safe
+import json
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class RoomView(TemplateView):
+    template_name = 'room.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(RoomView, self).get_context_data(**kwargs)
+
+        # Removendo inseguranças do JSON
+        context['room_json'] = mark_safe(
+            json.dumps(self.kwargs)
+        )
+        return context
+
+
