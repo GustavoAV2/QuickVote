37bae324a20aa7e142425ecb9f67dec0d379d057
gustavo.voltolini <1Cachorro.grande>
2021-09-16 17:23:41 -0300

    Refactoring consumers.py

--- quickvote/consumers.py
+++ quickvote/consumers.py
@@ -21,43 +21,44 @@ class RoomConsumer(WebsocketConsumer):
 
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name
+            self.room_group_name, self.channel_name
         )
         self.accept()
-        self.receive(text_data=json.dumps({'name': self.username, 'ready': False, 'vote': ''}))
+        self.receive(text_data=json.dumps(
+            {'command': 'update_user', 'name': self.username, 'ready': False, 'vote': ''}
+        ))
 
     def disconnect(self, close_code):
         # Leave room group
         async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
+            self.room_group_name, self.channel_name
         )
         self.actions.disconnect_room(self.room_name, self.username)
         self.receive(text_data=json.dumps({'name': self.username, 'ready': False, 'vote': ''}))
 
     # Receive message from WebSocket
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-
-        ready = text_data_json.get('ready')
-        name = text_data_json.get('name')
-        vote = text_data_json.get('vote')
+        data_json = json.loads(text_data)
+        command = data_json.get('command')
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
-            {
-                'type': 'update_room',
-                'room': self.room_name,
-                'ready': ready,
-                'vote': vote,
-                'name': name,
-            }
+            {'type': command, **data_json}
         )
 
-    # Receive message from room group
-    def update_room(self, event):
-        room = self.actions.refresh_room(room=event.get('room'), user=event)
-        # Send message to WebSocket
+    def update_user(self, event):
+        room = self.actions.refresh_room(room=self.room_name, user=event)
         self.send(text_data=json.dumps(room.serialize()))
+
+    # Start vote from room group
+    def stop_or_run_room(self, event):
+        if event.get('admin'):
+            room = self.actions.scenery.get_room_by_number(self.room_name)
+
+            if room.started:
+                room.finish_server()
+            else:
+                room.start_server()
+
+            self.send(text_data=json.dumps(room.serialize()))
