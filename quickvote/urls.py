3b27668a26e0accf9efe8d730e1e664f075aed4a
gustavo.voltolini <1Cachorro.grande>
2021-09-16 23:07:32 -0300

    Adding static in urls

--- quickvote/urls.py
+++ quickvote/urls.py
@@ -1,8 +1,10 @@
 # /urls.py
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from quickvote.views import RoomView, IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('<str:room_name>/<str:username>/', RoomView.as_view(), name='room'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
