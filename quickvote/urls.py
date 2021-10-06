96559c398c17f09245042b9e12fa71bcc6e0082a
gustavo.voltolini <1Cachorro.grande>
2021-10-06 11:46:52 -0300

    Creating view and URL of FastLogin

--- quickvote/urls.py
+++ quickvote/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from quickvote.views import RoomView, IndexView
+from quickvote.views import RoomView, IndexView, FastLogin
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
+    path('fast-login/<str:room_name>', FastLogin.as_view(), name='fast-login'),
     path('<str:room_name>/<str:username>/', RoomView.as_view(), name='room'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
