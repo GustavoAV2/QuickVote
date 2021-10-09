6ef6cfb63565d14f8dfc47964865ac25993d03b4
gustavo.voltolini <1Cachorro.grande>
2021-10-09 16:59:03 -0300

    Adding password to routes

--- quickvote/urls.py
+++ quickvote/urls.py
@@ -6,6 +6,6 @@ from quickvote.views import RoomView, IndexView, FastLogin
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
-    path('fast-login/<str:room_name>', FastLogin.as_view(), name='fast-login'),
-    path('<str:room_name>/<str:username>/', RoomView.as_view(), name='room'),
+    path('fast-login/<str:room_name>/<str:password>', FastLogin.as_view(), name='fast-login'),
+    path('<str:room_name>/<str:username>/<str:password>', RoomView.as_view(), name='room'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
