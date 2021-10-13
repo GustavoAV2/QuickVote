6ecde5573e21b72c0a63bac8808b75f46c5ad794
gustavo.voltolini <1Cachorro.grande>
2021-10-13 08:35:39 -0300

    Creating actions tests

--- tests/quickvote/test_actions.py
+++ tests/quickvote/test_actions.py
@@ -48,15 +48,61 @@ class TestScenery:
 
     def test_actions_connect_room(self):
         actions = Actions()
+        username = 'UserTest'
+
+        actions.create_room_for_users(self.room.get('room'), self.room.get('theme'), self.room.get('password'))
+
+        room = actions.connect_room(username, self.room.get('room'), self.room.get('password'))
+
+        assert room.get_user_by_name(username).name == username
 
     def test_actions_disconnect_room(self):
         actions = Actions()
+        user = User(name='UserTest', room=self.room.get('room'))
+        room = actions.create_room_for_users(
+            self.room.get('room'), self.room.get('theme'), [user], self.room.get('password')
+        )
+
+        actions.disconnect_room(user.room, user.name)
+
+        assert not len(room.users)
+        assert not actions.scenery.get_room_by_room_name(room.room_name)
 
     def test_actions_start_votes(self):
         actions = Actions()
+        user = User(name='UserTest', room=self.room.get('room'))
+        room = actions.create_room_for_users(self.room.get('room'), self.room.get('theme'),
+                                             [user], self.room.get('password'))
+
+        start_user_not_ready = actions.start_votes(room.room_name).started
+        user.ready = True
+        start_user_ready = actions.start_votes(room.room_name).started
+
+        assert not start_user_not_ready
+        assert start_user_ready
 
     def test_actions_finalize_votes(self):
         actions = Actions()
+        user = User(name='UserTest', room=self.room.get('room'))
+        user.ready = True
+        room = actions.create_room_for_users(self.room.get('room'), self.room.get('theme'),
+                                             [user], self.room.get('password'))
+
+        start_response = actions.start_votes(room.room_name).started
+        finished_response = actions.finalize_votes(room.room_name).started
+
+        assert start_response
+        assert not finished_response
 
     def test_actions_refresh_room(self):
         actions = Actions()
+        user = User(name='UserTest', room=self.room.get('room'))
+        actions.create_room_for_users(
+            self.room.get('room'), self.room.get('theme'), [user], self.room.get('password')
+        )
+
+        user.ready = True
+        user.vote = user.name
+        response = actions.refresh_room(room=user.room, user=user.serialize()).serialize()
+        response_clear = actions.refresh_room(room=user.room, user=user.serialize(), start=True).serialize()
+
