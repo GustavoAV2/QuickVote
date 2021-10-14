9622a454f48514f452080c57dfb9de03a59af94b
gustavo.voltolini <1Cachorro.grande>
2021-10-14 10:41:08 -0300

    Correcting called function

--- staticfiles/staticfiles/quickvote/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-from django.urls import re_path, path
-from quickvote.consumers import RoomConsumer
-
-websocket_urlpatterns = [
-    # re_path(r'^ws/room/(?P<room_name>\w+)/(?P<username>\w+)/$', RoomConsumer),
-    path('ws/room/<str:room_name>/<str:username>/<str:password>', RoomConsumer),
-]
