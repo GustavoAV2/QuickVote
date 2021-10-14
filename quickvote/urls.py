1272a3a422396ef78940acece23d78106cc2d447
gustavo.voltolini <1Cachorro.grande>
2021-10-13 22:23:11 -0300

    Correcting urls

--- quickvote/urls.py
+++ quickvote/urls.py
@@ -1,11 +1,9 @@
 # /urls.py
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from quickvote.views import RoomView, IndexView, FastLogin
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
     path('fast-login/<str:room_name>/<str:password>', FastLogin.as_view(), name='fast-login'),
     path('<str:room_name>/<str:username>/<str:password>', RoomView.as_view(), name='room'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
