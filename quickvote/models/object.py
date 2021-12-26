174a3f5add119854fb511f3c7cedc23787fc9f43
Gustavo Voltolini <gustavo.voltolini@ambevtech.com.br>
2021-12-26 20:35:29 -0300

    Creating new object

--- quickvote/models/object.py
+++ quickvote/models/object.py
@@ -1,3 +1,4 @@
+from quickvote.models.type_votes import Fibonacci
 
 
 class Object:
@@ -14,4 +15,24 @@ class Object:
             'name': self.name,
             'description': self.description,
             'number_of_votes': self.number_of_votes
+        }
+
+
+class ObjectPlanning(Object):
+    fib = Fibonacci
+
+    def __init__(self, name, number_of_votes, description):
+        super().__init__(name, number_of_votes, description)
+        self.votes = []
+
+    def clear_votes(self):
+        self.number_of_votes = 0
+        self.votes = []
+
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'number_of_votes': self.number_of_votes,
+            'vote': self.votes
         }
